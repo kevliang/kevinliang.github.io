d612dddc82346278b0a7fa3f807e1c2b41a0ef0d
Kevin Liang <kevinliang@Kevins-MacBook-Air.local>
2020-01-16 13:47:50 -0800

    Jan 16th - added results section

--- src/infographic.js
+++ src/infographic.js
@@ -6,10 +6,8 @@ function runInfographic (){
     	animateValue(objectCity, 0, objectCity.innerHTML, 500);
 		// Set the values for the different charts and labels
 
-		console.log(objectEmission);
+		console.log(map.emission);
 
-		$("#chart1_bar2 .label").text(totalEmission);
-		$("#chart1_bar1 .label").text($("#td3_2").text());
 		$("#2009aid").text("$"+$("#td4_2").text()+"m");
 		$("#2010aid").text("$"+$("#td4_3").text()+"m");
 		
@@ -17,22 +15,16 @@ function runInfographic (){
 		// Store the values from the table
 	
 		bar1 = parseInt($("#td3_2").text());		
-		bar2 = parseInt($("#td2_2").text());
-		bar3 = parseInt($("#td3_3").text());
-		bar4 = parseInt($("#td2_3").text());	
+		bar2 = parseInt(map.emission);
 		
 		area1 = parseFloat($("#td4_2").text());
-		area2 = parseFloat($("#td4_3").text());
+		area2 = (map.emission)/5
 		
 		// Find the biggest value for the bar chart
 		
 		biggestbar = bar1;
 		if (bar2>biggestbar)
 			biggestbar = bar2;
-		if (bar3>biggestbar)
-			biggestbar = bar3;
-		if (bar4>biggestbar)
-			biggestbar = bar4;
 		
 		// Get the relative heights
 		
@@ -40,15 +32,11 @@ function runInfographic (){
 		
 		bar1height = (bar1/biggestbar) * maxheight;
 		bar2height = (bar2/biggestbar) * maxheight;
-		bar3height = (bar3/biggestbar) * maxheight;
-		bar4height = (bar4/biggestbar) * maxheight;
 		
 		// Set the height using CSS
 		
 		$("#chart1_bar1").delay(500).animate({height:bar1height},1000);
 		$("#chart1_bar2").delay(1000).animate({height:bar2height},1000);
-		$("#chart2_bar1").delay(1500).animate({height:bar3height},1000);
-		$("#chart2_bar2").delay(2000).animate({height:bar4height},1000);
 		
 		// Get the relative sizes
 		
@@ -59,14 +47,13 @@ function runInfographic (){
 		
 		// Set the area using CSS
 		
-		$("#chart3_area1").delay(2500).animate({height:area1width,width:area1width},1000);
 		$("#chart3_area2").delay(2500).animate({height:area2width,width:area2width},1000);
 		
 		// calculate percentage change of infections
 		
-		percentchangeinfections = parseInt(((1-((bar1+bar2)/(bar3+bar4)))*100)+100);
+		//percentchangeinfections = parseInt(((1-((bar1+bar2)/(bar3+bar4)))*100)+100);
 		//alert(percentchangeinfections+' percent drop');
-		$("#percentchange").html("<span>decrease of</span>"+percentchangeinfections+"%");
+		//$("#percentchange").html("<span>decrease of</span>"+percentchangeinfections+"%");
 		
 		
 		$("#barcharts").hover(function(){
