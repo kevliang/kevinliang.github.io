9e7ff6196119de14f48f05b27c75e491c92ab4c9
Kevin Liang <kevinliang@Kevins-MacBook-Air.local>
2020-02-09 23:35:38 -0800

    minified and things

--- src/emissions.js
+++
@@ -1,8 +0,0 @@
-var str = document.querySelector("#distancebetween").innerText;
-var res = Number(str.replace(/\D/g, "")); 
-var adjusteddistance = res*1.609344;  //miles to km
-
-
-//document.querySelector("#carbon").innerText = adjusteddistance;
-
-
